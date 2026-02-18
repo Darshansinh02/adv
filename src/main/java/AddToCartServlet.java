2a5943891f9b1a7cf1fc842d35060ca50e162da8
DARSHANSINH VASADIYA <vasadiyadarshansinh@gmail.com>
2026-02-18 04:43:29 +0000

    13

--- src/main/java/AddToCartServlet.java
+++ src/main/java/AddToCartServlet.java
@@ -1,8 +1,25 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+class Product {
+    int id;
+    String name;
+    double price;
+
+    Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
 
 public class AddToCartServlet extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int productId = Integer.parseInt(request.getParameter("productId"));
         int quantity = Integer.parseInt(request.getParameter("quantity"));
